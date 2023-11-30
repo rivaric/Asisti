440d167d80a754357639466f186e4f6c5f96b2ea
littowl <alina.kotyaeva@mail.ru>
2023-11-30 19:27:46 +0300

    🤡 picture

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import { useStyles } from './Card.styles'
+import exercise from '../../assets/exercise.jpg'
 
 interface CardProps {
   id: string
@@ -8,13 +9,14 @@ interface CardProps {
   img?: string
 }
 
-export function Card({ id, name, verbose_name, img }: CardProps) {
+export function Card({ id, name, verbose_name, }: CardProps) {
   const classes = useStyles()
   const navigate = useNavigate()
+
   return (
     <div className={classes.train} onClick={() => navigate(`/train/${id}`)}>
       {/* <div className={classes.descr}>15 мин • 5 упражнений</div> */}
-      <img src={img} />
+      <img src={exercise} width="80%" />
       <div className={classes.name}>{verbose_name || name}</div>
     </div>
   )
--- src/components/historyCard/HistoryCard.tsx
+++ src/components/historyCard/HistoryCard.tsx
@@ -2,6 +2,7 @@ import { useStyles } from './HistoryCard.styles'
 import { ArrowLaft } from '../../iconpack/ArrowLaft'
 import { useNavigate } from 'react-router-dom'
 import { Exercise } from '../../types'
+import img from '../../assets/exercise.jpg'
 
 export function HistoryCard({exercise}: {exercise: Exercise}) {
   const classes = useStyles()
@@ -12,7 +13,7 @@ export function HistoryCard({exercise}: {exercise: Exercise}) {
       <div className={classes.title}>История упражнений</div>
       <div className={classes.exercise} onClick={() => navigate(`/train/${exercise.id}`)}>
         <div className={classes.counter}>12/16</div>
-        <img src={exercise?.image_url} />
+        <img src={img} width="80%"style={{alignSelf: 'center'}} />
         <div className={classes.exerciseName}>{exercise?.verbose_name || exercise?.name}</div>
       </div>
       <div className={classes.toHistory} onClick={() => navigate('/history')}>
